ec5e605540791f2f2d84721ac121284180d5b760
Karl <95616812+HaciendaTuro@users.noreply.github.com>
2024-07-12 11:44:29 -0400

    Create Karl.cs
    Roadmap 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/Karl.cs
@@ -0,0 +1,50 @@
+// 1. Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+//https://learn.microsoft.com/es-es/dotnet/csharp/tour-of-csharp/tutorials/
+
+// 2. Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// En C#, existen tres tipos principales de comentarios:
+// Comentarios de una sola línea: Se inician con // y terminan al final de la línea.
+// Este es un comentario de una sola línea
+/*
+Comentarios de varias líneas: Se inician con /* y terminan con */
+/* Este es un comentario
+   de varias líneas */
+
+
+// 3. Crea una variable (y una constante si el lenguaje lo soporta).
+
+/*
+1. Variables estaticas Ej. public static int x;
+2. Variables de instancia Ej. public int y;
+3. Elementos Array Ej. int[] array = new int[5];
+                       array[0] = 1; // Elemento de array
+4. Parametro de valor Ej. void Metodo(int a) { }
+5. Parametro de referencia Ej. clave ref.
+6. Parametros de salida Ej. void Metodo(out int c) { c = 10; }
+7. Parametros de entrada Ej. void Metodo(in int d) { }
+8. Variables locales Ej. void Metodo(in int d) { }
+*/
+ 
+// 5. Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).   
+
+int entero =10;                   
+byte byteEntero = 1254;              
+short corto = 30000;                
+long largo = 1234567890123;           
+float flotante = 1.536f;              
+double doble = 3.14159;               
+decimal decimalValor = 123.456m       
+uint varaibleEnteroPositivo = 3500;
+ulong varibleEnteroPositivoGrande = 987654321;
+ushort variableEnteroPsitivoPequeño = 800;
+byte variableByte = 128;
+float variableNumeroFlotante = 2.5f;
+double variableNumeroFlotantePrecisionDoble = 2.5D;
+decimal variableNumeroDecimal = 2.5m;
+bool variableBooleano = true;
+bool variableBooleano = false;
+
+ Console.WriteLine("Hola C#");
+  
